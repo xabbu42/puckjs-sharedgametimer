403a587b9fad3bc00b7e6502370372420843e351
Nathan Gass <gass@search.ch>
2025-11-29 11:10:01 +0100

    move suggestions to head of file

--- sharedgametimer.js
+++ sharedgametimer.js
@@ -1,3 +1,20 @@
+var suggestions = {
+    script: [
+        '0 sgtState;sgtColor;sgtTurnTime;sgtPlayerTime%0A'
+    ],
+    scriptName: "Pill Button Write",
+    defaultTriggers: ["includePlayers","includePause","includeAdmin","includeSimultaneousTurns","includeGameStart","includeGameEnd","includeSandTimerStart","includeSandTimerReset","includeSandTimerStop","includeSandTimerOutOfTime","runOnStateChange","runOnPlayerOrderChange","runOnPoll","runOnBluetoothConnect","runOnBluetoothDisconnect"],
+    actionMap: [
+        ['Short Press 1', 'remoteActionPrimary'],
+        ['Short Press 2', 'remoteActionToggleAdmin'],
+        ['Long Press 0', 'remoteActionSecondary'],
+        ['Long Press 1', 'remoteActionTogglePause'],
+        ['Long Press', 'remoteActionUndo'],
+        ['Connected', 'remoteActionPoll']
+    ],
+    actionMapName: "Pill Button Actions"
+};
+
 // Blink green for 100ms
 function blinkGreen() {
 	LED2.write(true);
@@ -11,25 +28,7 @@ setWatch(blinkGreen, BTN, { edge: "rising", repeat: true, debounce: 50 });
 
 // Send configuration on Bluetooth connect
 NRF.on('connect', function(addr) {
-    var suggestions = {
-        script: [
-            '0 sgtState;sgtColor;sgtTurnTime;sgtPlayerTime%0A'
-        ],
-        scriptName: "Pill Button Write",
-        defaultTriggers: ["includePlayers","includePause","includeAdmin","includeSimultaneousTurns","includeGameStart","includeGameEnd","includeSandTimerStart","includeSandTimerReset","includeSandTimerStop","includeSandTimerOutOfTime","runOnStateChange","runOnPlayerOrderChange","runOnPoll","runOnBluetoothConnect","runOnBluetoothDisconnect"],
-        actionMap: [
-            ['Short Press 1', 'remoteActionPrimary'],
-            ['Short Press 2', 'remoteActionToggleAdmin'],
-            ['Long Press 0', 'remoteActionSecondary'],
-            ['Long Press 1', 'remoteActionTogglePause'],
-            ['Long Press', 'remoteActionUndo'],
-            ['Connected', 'remoteActionPoll']
-        ],
-        actionMapName: "Pill Button Actions"
-    };
-    
-    Bluetooth.println(JSON.stringify(suggestions));
-    print("Config sent to " + addr);
+    print(JSON.stringify(suggestions));
 });
 
 // Transmit Bluetooth Low Energy advertising packets
